328786227417239ab643e6a4dd03fd65823423af
Marko Ambrožič <marko.ambrozic@pju.si>
2024-01-19 16:06:35 +0100

    Add validation debug echo

--- validate_service_json.php
+++ validate_service_json.php
@@ -2,6 +2,8 @@
 $rootDir = getcwd(); // Gets the root directory of the project
 $serviceFile = $rootDir . '/service.json';
 
+echo "Running service.json validation" . PHP_EOL;
+
 if (!file_exists($serviceFile)) {
     fwrite(STDERR, "Error: 'service.json' not found in the project root.\n");
     exit(1); // Exit with an error code
